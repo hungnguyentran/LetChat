a9c942b46d5212c64d1fca7ec73b69de78f33ae9
joelnguyen <hungnguyentran1992@gmail.com>
2021-06-03 20:54:54 +1000

    Create login_page.dart
    add login page

---
+++ lib/pages/login/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('Login Page'),
+      )
+    );
+
+  }
+}
